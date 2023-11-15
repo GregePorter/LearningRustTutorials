7378f9b7f1f2c0c7707822359bf6f9098fc937cd
Gregory Porter <greg.e.porter@gmail.com>
2023-11-14 21:51:26 -0600

    continued the struct section

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,22 @@
-fn main() {
-    let rec1 = (30, 50);
-
-    println!("The area of the rectangle is {} square pixels.", area(rec1));
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn main() {
+    let scale = 2;
+    let rec1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+
+    dbg!(&rec1);
+    println!("rec1 is {:?}.", rec1);
+    println!("The area of the rectangle is {} square pixels.", area(&rec1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
